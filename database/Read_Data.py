cef797acae52cea70abc1b672f1169d2d6ed9bc9
Ali <atajadod94@gmail.com>
2018-06-05 19:48:45 -0400

    removing double header
    #26
    I believe the most efficent way to do this would be to include an option in the setting for the possibility of double headers.   In any case , it now works.

--- database/Read_Data.py
+++ database/Read_Data.py
@@ -2,7 +2,8 @@ import json
 import pandas as pd
 import numpy as np
 import os
-from scipy.interpolate import interp1d,interp2d
+from scipy.interpolate import interp1d, interp2d
+
 
 ## Read Settings
 ## Use Settings
@@ -20,32 +21,34 @@ def set_data(data_adress, setting_locator, setting):
     with open(set_name, 'r') as fp:
         setting = json.loads(fp.read())
 
-
     if setting['Header']:
         setting['Header'] = eval(setting['Header'])
         for idx, item in enumerate(setting['Header']):
             if item is '':
                 setting['Header'][idx] = 'Unused' + str(idx)
         # remove existing header first, need to ask for this in settings in the read with rows = 1::
-        data = pd.read_csv(data_adress, sep='\t', skiprows=[0], names=setting['Header'])
-        #setting['outputheaders'] = data.iloc[0]
-        #data.drop(data.index[0], inplace=True)
+        data = pd.read_csv(data_adress, sep='\t', names=setting['Header'])
+        try:
+            int(data.iloc[0][0])
+        except ValueError:
+            data = pd.read_csv(data_adress, sep='\t', skiprows=[0], names=setting['Header'])
 
-        #data.drop(0, inplace = True) as commented here
+
 
 
     else:
         data = pd.read_csv(data_adress, sep='\t')
 
-    unify_data(data, setting) #this is to set the columns and units
+    unify_data(data, setting)  # this is to set the columns and units
     return set_experiment(data, setting), setting
+
 
 def set_experiment(data, setting):
     cfg = {}
     cfg['Trial'] = {}
     output_data = data.copy()
     output_data_header = list(output_data.keys())
-    #for idx, item in enumerate(output_data_header):
+    # for idx, item in enumerate(output_data_header):
     #    if item.startswith('Unused'):
     #        data.columns.values[idx] = setting['outputheaders'][idx]
     output_data['accept'] = 0
@@ -57,12 +60,10 @@ def set_experiment(data, setting):
     cfg['all_targets'] = target_locations
     cfg['output'] = output_data
 
-
-
     step_start = setting['Segments'][0]
     step_end = setting['Segments'][1]
 
-    for name, group in data.groupby(['trial_no']):   ##looking through trials
+    for name, group in data.groupby(['trial_no']):  ##looking through trials
         if step_start is '':
             group.selected = 1
         else:
@@ -70,21 +71,23 @@ def set_experiment(data, setting):
             step_end = int(step_end)
             cfg['Trial'][name] = set_trial(group, step_start, step_end)
 
-
     return cfg
 
+
 def set_trial(trial, step_start, step_end):
-        segment = trial.index[trial.step.between(int(step_start), int(step_end))]
-        trial.loc[segment, 'selected'] = 1
-        return trial
+    segment = trial.index[trial.step.between(int(step_start), int(step_end))]
+    trial.loc[segment, 'selected'] = 1
+    return trial
+
 
 def unify_data(data, setting):
-    data.dropna(inplace=True)  #remove any rows or columns with Nan's (maybe add a special pop up for this? some notice)
+    data.dropna(
+        inplace=True)  # remove any rows or columns with Nan's (maybe add a special pop up for this? some notice)
     if setting['Display Origin'] == ['', '', '']:
-        setting['Display Origin']  = [528, 395, 'px']
+        setting['Display Origin'] = [528, 395, 'px']
 
     if ~ len(setting['Header']) == len(data.columns):
-        assert('Header columns and data columns do not match')
+        assert ('Header columns and data columns do not match')
 
     else:
         setting_headers = data.columns
@@ -99,20 +102,22 @@ def unify_data(data, setting):
                         data['time_ms'] = data.time_m.astype('float') / 60000
                         data.drop('time_m', axis=1, inplace=True)
                     else:
-                        assert('I don''t know how to handle this unit for time:  ' + unit)
+                        assert ('I don''t know how to handle this unit for time:  ' + unit)
                 if unit == 'ms':
                     data['time_ms'] = data.time_ms.astype('float')
 
             if key.startswith('cursorx'):
                 unit = key.split('_')[1]
                 if unit == 'px':
-                    data['cursorx_cm'] = (data.cursorx_px.astype('float') - setting['Display Origin'][0]) * float(setting['PX_CM_Ratio'])
+                    data['cursorx_cm'] = (data.cursorx_px.astype('float') - setting['Display Origin'][0]) * float(
+                        setting['PX_CM_Ratio'])
                     data.drop('cursorx_px', axis=1, inplace=True)
 
             if key.startswith('cursory'):
                 unit = key.split('_')[1]
                 if unit == 'px':
-                    data['cursory_cm'] = (data.cursory_px.astype('float') - setting['Display Origin'][1]) * float(setting['PX_CM_Ratio'])
+                    data['cursory_cm'] = (data.cursory_px.astype('float') - setting['Display Origin'][1]) * float(
+                        setting['PX_CM_Ratio'])
                     data.drop('cursory_px', axis=1, inplace=True)
 
             if key.startswith('penx'):
@@ -129,12 +134,13 @@ def unify_data(data, setting):
             if key.startswith('targetx'):
                 unit = key.split('_')[1]
                 if unit == 'px':
-                    data['targetx_cm'] = (data.targetx_px.astype('float') - setting['Display Origin'][0]) * float(setting['PX_CM_Ratio'])
+                    data['targetx_cm'] = (data.targetx_px.astype('float') - setting['Display Origin'][0]) * float(
+                        setting['PX_CM_Ratio'])
                     data.drop('targetx_px', axis=1, inplace=True)
 
             if key.startswith('targety'):
                 unit = key.split('_')[1]
                 if unit == 'px':
-                    data['targety_cm'] = (data.targety_px.astype('float') - setting['Display Origin'][1]) * float(setting['PX_CM_Ratio'])
+                    data['targety_cm'] = (data.targety_px.astype('float') - setting['Display Origin'][1]) * float(
+                        setting['PX_CM_Ratio'])
                     data.drop('targety_px', axis=1, inplace=True)
-
